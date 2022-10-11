8978ad9ed72d46870048ca5fe044a0b45c5ab553
parkjungmin11 <101533172+parkjungmin11@users.noreply.github.com>
2022-10-11 13:48:16 +0900

    첫 프로젝트생성

---
+++ src/main/java/com/example/study/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String index() {
+        return "index";
+    }
+}
